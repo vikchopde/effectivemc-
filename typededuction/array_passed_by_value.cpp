d7a96dc8dc64d0eefbbb60e1aa1b8cffbad1c26a
vikchopde <chopde.vikram@gmail.com>
2017-12-25 01:38:13 +0800

    Initial commit

---
+++ typededuction/array_passed_by_value.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "utility.h"
+
+/*
+ * Key Idea:
+ *
+ *    template deduction : void foo<char*>(char*)
+ */
+
+template<typename T>
+void foo(T& param); //{}
+
+MAIN_FUNC
+{
+    char buff[] = "some buffer.!";
+    foo(buff);
+};
