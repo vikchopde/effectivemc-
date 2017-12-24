84a890bcb0ad0f825cb281bd758ad4b4706fd63d
vikchopde <chopde.vikram@gmail.com>
2017-12-25 01:13:04 +0800

    Initial commit

---
+++ typededuction/array_decay_rule.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "utility.h"
+
+/*
+ * Key Idea:
+ *          In many contexts , Array decay into pointer .
+ *
+ */
+
+
+MAIN_FUNC
+{
+    char arr[] = { "vikram"};     //char[]
+    char *name = arr;             //decays to char*
+
+    std::cout << name << std::endl;
+};
