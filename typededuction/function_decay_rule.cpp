c803e5bc2cd38a37a0367545c36972a88cb6e0ee
vikchopde <chopde.vikram@gmail.com>
2017-12-25 01:21:27 +0800

    Initial commit

---
+++ typededuction/function_decay_rule.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "utility.h"
+
+/*
+ * Key Idea:
+ *
+ *      function type also decay into pointers.
+ *      and everything which is true for array type deduction also applies to function type deduction.
+ */
+
+void somefunc(int, double) {}
+
+template <typename T>
+void f(T param) {}
+
+template<typename T>
+void f2(T& param) {}
+
+MAIN_FUNC
+{
+    f(somefunc);   // param will deduce to : void (*)(int, double)
+
+    f2(somefunc);  // param will deduce to : void (&)(int, double)
+};
